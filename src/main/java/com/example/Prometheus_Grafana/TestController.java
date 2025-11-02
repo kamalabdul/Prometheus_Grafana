dbf19976d97dd7dea22890c7dc96f490c33ce0a5
kamalabdul <kamal.abdul@gmail.com>
2025-11-02 09:59:40 +0800

    Initial commit

---
+++ src/main/java/com/example/Prometheus_Grafana/TestController.java
@@ -0,0 +1,14 @@
+package com.example.Prometheus_Grafana;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/monitor")
+	public String monitor() {
+		return "Hello World";
+
+	}
+}
