b7529eaaedf6737f7cf01f3c2453d1cc2baaf1f3
kamalabdul <kamal.abdul@gmail.com>
2025-11-02 12:28:45 +0800

    add try

--- src/main/java/com/example/Prometheus_Grafana/PrometheusGrafanaApplication.java
+++ src/main/java/com/example/Prometheus_Grafana/PrometheusGrafanaApplication.java
@@ -7,7 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PrometheusGrafanaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrometheusGrafanaApplication.class, args);
+		
+		
+		//SpringApplication.run(PrometheusGrafanaApplication.class, args);
+		
+		try {
+		    SpringApplication.run(PrometheusGrafanaApplication.class, args);
+		    System.out.println("Application started successfully");
+		} catch (Exception e) {
+			System.out.println("Failed to start application: " +  e.getMessage());
+		    System.exit(1);
+		}
+		
+		
 	}
 
 }
+
+
